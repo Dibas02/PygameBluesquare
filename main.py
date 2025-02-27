4f9e7b9b08ccb885acdf74c9ea7f3b6b0578e98e
Dibas Talukder <debal1952@gmail.com>
2025-02-27 20:03:47 +0600

    initial commit

--- main.py
+++ main.py
@@ -2,19 +2,59 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 300))
+screen = pygame.display.set_mode((500, 500))
+screen_width, screen_height = 500, 500
+
+colors = {
+
+    'red':pygame.Color('red'),
+    'blue':pygame.Color('blue'),
+    'green':pygame.Color('green'),
+    'yellow':pygame.Color('yellow'),
+    'white':pygame.Color('white')
+    }
+current_color = colors['white']
+
+x, y = 30, 30
+
+sprite_width, sprite_height = 60, 60
+
+clock = pygame.time.Clock()
+
+
+
 done = False
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    pygame.draw.rect(screen, (0, 125, 225), pygame.Rect(30, 30, 60, 60))
 
-    Green = (0, 225, 0)
-    pygame.draw.circle(screen, Green, (250, 250), 50)
-    pygame.draw.circle(screen, Green, (250, 200), 50, 3)
+    pressed = pygame.key.get_pressed()
+
+    if pressed[pygame.K_LEFT]: x -= 3
+    if pressed[pygame.K_RIGHT]: x += 3
+    if pressed[pygame.K_DOWN]: y -= 3
+    if pressed[pygame.K_UP]: y += 3
+
+
+    x = min(max(0, x), screen_width - sprite_width)
+    y = min(max(0, y), screen_height - sprite_height)
+
+
+
+    if x == 0: current_color = colors['blue']
+    elif x == screen_width - sprite_width: current_color = colors['yellow']
+
+    elif y == 0: current_color = colors['red']
+    elif y == screen_height - sprite_height: current_color = colors['green']
+    else: current_color = colors['white']
+
+    screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, current_color, (x, y, sprite_width, sprite_height))
+
     
 
-
 pygame.display.flip()
+clock.tick(90)
